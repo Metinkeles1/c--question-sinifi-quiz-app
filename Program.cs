e050621dc2a272ad7b29d2abbaa2c9959cae02e6
Metinkeles1 <97405379+Metinkeles1@users.noreply.github.com>
2022-08-18 11:20:33 +0300

    update

--- Program.cs
+++ Program.cs
@@ -18,6 +18,55 @@
         }
     }
 
+    class Quiz
+    {
+        public Quiz(Question[] questions)
+        {
+            this.Questions = questions;
+            this.QuestionIndex = 0;
+        }
+        private Question[] Questions { get; set; }
+
+        public int QuestionIndex { get; set; }
+
+        public Question GetQuestion()
+        {
+            return this.Questions[this.QuestionIndex];
+        }
+
+        public void DisplayQuestion()
+        {
+            var question = this.GetQuestion();
+            Console.WriteLine($"soru {this.QuestionIndex + 1}: {question.Text}");
+
+            foreach (var c in question.Choices)
+            {
+                Console.WriteLine($"-{c}");
+            }
+            Console.Write("cevap: ");
+            var cevap = Console.ReadLine();
+            this.Guess(cevap);
+        }
+
+        public void Guess(string answer)
+        {
+            var question = this.GetQuestion();
+            Console.WriteLine(question.checkAnswer(answer));
+            this.QuestionIndex++;
+
+            if (this.Questions.Length == this.QuestionIndex)
+            {
+                //skor
+                return;
+            }
+            else
+            {
+                this.DisplayQuestion();
+            }
+
+        }
+    }
+
     static void Main(string[] args)
     {
         var q1 = new Question("En iyi programlama dili hangisidir?", new string[] { "C#", "Python", "Java", "C++" }, "C#");
@@ -25,21 +74,8 @@
         var q3 = new Question("En kazandıran programlama dili hangisidir?", new string[] { "C#", "Python", "Java", "C++" }, "C#");
 
         var questions = new Question[] { q1, q2, q3 };
+        var quiz = new Quiz(questions);
 
-        int index = 1;
-        foreach (var q in questions)
-        {
-            Console.WriteLine($"soru {index}: {q.Text}");
-            index++;
-
-            foreach (var c in q.Choices)
-            {
-                Console.WriteLine($"-{c}");
-            }
-
-            Console.Write("cevap: ");
-            var cevap = Console.ReadLine();
-            Console.WriteLine(q.checkAnswer(cevap));
-        }
+        quiz.DisplayQuestion();
     }
 }
