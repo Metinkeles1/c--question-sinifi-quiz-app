cff60d12429523d77c0ae01dfde5fb9c7cfbd71f
Metinkeles1 <97405379+Metinkeles1@users.noreply.github.com>
2022-08-19 13:04:29 +0300

    update

--- Program.cs
+++ Program.cs
@@ -1,72 +1,91 @@
-﻿class Program
+﻿class Question
 {
-    class Question
+    public Question(string text, string[] choices, string answer)
     {
-        public Question(string text, string[] choices, string answer)
-        {
-            this.Text = text;
-            this.Choices = choices;
-            this.Answer = answer;
-        }
-        public string Text { get; set; }
-        public string[] Choices { get; set; }
-        public string Answer { get; set; }
+        this.Text = text;
+        this.Choices = choices;
+        this.Answer = answer;
+    }
+    public string Text { get; set; }
+    public string[] Choices { get; set; }
+    public string Answer { get; set; }
 
-        public bool checkAnswer(string answer)
+    public bool checkAnswer(string answer)
+    {
+        return this.Answer.ToLower() == answer.ToLower();
+    }
+}
+
+class Quiz
+{
+    public Quiz(Question[] questions)
+    {
+        this.Questions = questions;
+        this.QuestionIndex = 0;
+        this.Score = 0;
+    }
+    private Question[] Questions { get; set; }
+
+    private int QuestionIndex { get; set; }
+
+    private int Score { get; set; }
+
+    private Question GetQuestion()
+    {
+        return this.Questions[this.QuestionIndex];
+    }
+
+    public void DisplayQuestion()
+    {
+        var question = this.GetQuestion();
+        this.DisplayProgress();
+        Console.WriteLine($"soru {this.QuestionIndex + 1}: {question.Text}");
+
+        foreach (var c in question.Choices)
         {
-            return this.Answer.ToLower() == answer.ToLower();
+            Console.WriteLine($"-{c}");
+        }
+        Console.Write("cevap: ");
+        var cevap = Console.ReadLine();
+        this.Guess(cevap);
+    }
+
+    private void Guess(string answer)
+    {
+        var question = this.GetQuestion();
+        if (question.checkAnswer(answer))
+            this.Score++;
+        this.QuestionIndex++;
+
+        if (this.Questions.Length == this.QuestionIndex)
+        {
+            this.DisplayScore();
+        }
+        else
+        {
+            this.DisplayQuestion();
         }
     }
 
-    class Quiz
+    private void DisplayScore()
     {
-        public Quiz(Question[] questions)
-        {
-            this.Questions = questions;
-            this.QuestionIndex = 0;
-        }
-        private Question[] Questions { get; set; }
-
-        public int QuestionIndex { get; set; }
-
-        public Question GetQuestion()
-        {
-            return this.Questions[this.QuestionIndex];
-        }
-
-        public void DisplayQuestion()
-        {
-            var question = this.GetQuestion();
-            Console.WriteLine($"soru {this.QuestionIndex + 1}: {question.Text}");
-
-            foreach (var c in question.Choices)
-            {
-                Console.WriteLine($"-{c}");
-            }
-            Console.Write("cevap: ");
-            var cevap = Console.ReadLine();
-            this.Guess(cevap);
-        }
-
-        public void Guess(string answer)
-        {
-            var question = this.GetQuestion();
-            Console.WriteLine(question.checkAnswer(answer));
-            this.QuestionIndex++;
-
-            if (this.Questions.Length == this.QuestionIndex)
-            {
-                //skor
-                return;
-            }
-            else
-            {
-                this.DisplayQuestion();
-            }
-
-        }
+        Console.WriteLine($"Score:{this.Score}");
     }
 
+    private void DisplayProgress()
+    {
+        int totalQuestion = this.Questions.Length;
+        int questionNumber = this.QuestionIndex + 1;
+
+        if (totalQuestion >= questionNumber)
+            Console.WriteLine($"Question {questionNumber} of {totalQuestion}");
+    }
+
+
+}
+
+class Program
+{
     static void Main(string[] args)
     {
         var q1 = new Question("En iyi programlama dili hangisidir?", new string[] { "C#", "Python", "Java", "C++" }, "C#");
